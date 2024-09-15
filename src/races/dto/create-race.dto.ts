b0e6927365a174a2e7d0d3c8bebb20af32374105
Sam Norris-Rogers <samnrogers@gmail.com>
2024-09-15 17:46:02 +0200

    create and update race
    Co-authored-by: Quentin Watt <quentin@qcasts.com>

--- src/races/dto/create-race.dto.ts
+++ src/races/dto/create-race.dto.ts
@@ -1,1 +1,15 @@
-export class CreateRaceDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateRaceDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  country: string;
+
+  @IsString()
+  @IsNotEmpty()
+  date: string;
+}
